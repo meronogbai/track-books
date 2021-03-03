2a2e9c50e3c7d670631730c19db92d8fbf4849e5
Meron Ogbai <okbaymeron@gmail.com>
2021-03-03 23:46:26 +0300

    Add a form to add book comp

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { changeTitle } from '../redux/title';
@@ -7,6 +7,9 @@ import Nav from './Nav';
 const Home = () => {
   const history = useHistory();
   const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [totalChapters, setTotalChapters] = useState('');
+  const [completedChapters, setCompletedChapters] = useState('');
   const token = localStorage.getItem('token');
   useEffect(() => {
     dispatch(changeTitle('Add Book'));
@@ -14,9 +17,50 @@ const Home = () => {
       history.push('/signup');
     }
   }, []);
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
   return (
     <>
-      <h1>Hello world</h1>
+      <form className="Form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="title"
+          id="title"
+          required
+          minLength="2"
+          maxLength="50"
+          value={title}
+          onChange={e => { setTitle(e.target.value); }}
+          placeholder="Title"
+          aria-label="Title"
+        />
+        <input
+          type="number"
+          name="total_chapters"
+          id="total_chapters"
+          required
+          min="1"
+          max="999"
+          value={totalChapters}
+          onChange={e => { setTotalChapters(e.target.value); }}
+          placeholder="Total Chapters"
+          aria-label="Total Chapters"
+        />
+        <input
+          type="number"
+          name="completed_chapters"
+          id="completed_chapters"
+          required
+          min="0"
+          max={totalChapters}
+          value={completedChapters}
+          onChange={e => { setCompletedChapters(e.target.value); }}
+          placeholder="Completed Chapters"
+          aria-label="Completed Chapters"
+        />
+        <button type="submit" className="Btn">Add Book</button>
+      </form>
       <Nav />
     </>
   );
