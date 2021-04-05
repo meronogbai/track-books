590a781ed43d47b399ff755fff519ae27f067ce3
Meron Ogbai <okbaymeron@gmail.com>
2021-04-05 05:20:50 +0300

    Update tests

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -3,6 +3,7 @@ import * as reactRedux from 'react-redux';
 import Header from '../components/Header';
 
 const useSelectorMock = jest.spyOn(reactRedux, 'useSelector');
+jest.spyOn(reactRedux, 'useDispatch');
 
 useSelectorMock.mockReturnValue('Header');
 
